2657db459bb1c4c9c22409f02747a73736b195d7
Bartek <250943@edu.p.lodz.pl>
2024-06-20 22:06:58 +0200

    queue

---
+++ queue.h
@@ -0,0 +1,11 @@
+
+#ifndef UNTITLED44_QUEUE_H
+#define UNTITLED44_QUEUE_H
+
+int isFull();
+int isEmpty();
+void insertQueue(int value);
+int deQueue();
+void displayQueue();
+
+#endif //UNTITLED44_QUEUE_H
